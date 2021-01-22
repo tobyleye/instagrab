410026bc804fcfeeb29ef54785ce6f916d61a077
toby <krisella74@gmail.com>
2021-01-22 20:28:46 +0100

    code backend

---
+++ ig.py
@@ -0,0 +1,59 @@
+import requests
+import re
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:84.0) Gecko/20100101 Firefox/84.0'}
+BASE_URL = 'https://www.instagram.com/p/{}/?__a=1'
+
+
+def node_extractor(node):
+    __type = node['__typename']
+    is_video = node['is_video']
+    url = node['video_url'] if is_video else node['display_url']
+    return {'__type': __type, 'is_video': is_video, 'download_url': url}
+
+
+def scrape_post(shortcode):
+    response = requests.get(BASE_URL.format(shortcode), headers=headers)
+    response.raise_for_status()
+    data = response.json()
+    with open('debug.html', 'w') as f:
+        f.write(response.text)
+    parent_node = data['graphql']['shortcode_media']
+    caption = parent_node['edge_media_to_caption'].get('edges', [])
+    if len(caption) > 0:
+        caption = caption[0]['node']['text']
+    else:
+        caption = ''
+    children_node = parent_node.get('edge_sidecar_to_children')
+    owner = {'username': parent_node['owner']['username'],
+            'full_name': parent_node['owner']['full_name'],
+            'avatar': parent_node['owner']['profile_pic_url'],
+        }
+
+    if parent_node['__typename'] == 'GraphSidecar' and children_node:
+        posts = [node_extractor(edge['node']) 
+                    for edge in children_node['edges']]
+    else:
+        posts = [node_extractor(parent_node)]
+    return {'posts': posts, 'owner': owner, 'caption': caption}
+
+def extract_shortcode_from_url(url):
+    match=re.search('instagram.com/p/([^/?]*)', url)
+    if match:
+        return match.group(1).strip('/')
+
+def run(url):
+    shortcode=extract_shortcode_from_url(url)
+    if shortcode:
+        return scrape_post(shortcode)
+
+if __name__ == "__main__":
+    import sys
+    import pprint
+    if len(sys.argv) > 1:
+        url=sys.argv[1]
+        pprint.pprint(run(url))
+    else:
+        print('wrong use of script')
+        sys.exit(0)
---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+from dotenv import load_dotenv
+import logging
+import ig
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world'
+
+@app.route('/api')
+def scrape_post():
+    url  = request.args.get('url')
+    if not url:
+        return {'message': 'url is missing'}, 400
+    # validate url while attempting to extract the post shortcode from url
+    post_shortcode = ig.extract_shortcode_from_url(url)
+    if not post_shortcode:
+        return { 'message': 'url is invalid' }, 400
+    try:
+        response = ig.scrape_post(post_shortcode)
+        return response
+    except Exception as err:
+        logging.exception('An internal error occured {}'.format(err))
+        return { 'message': 'an unknown error occured', 'error': '{}'.format(err) }, 500
+        
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occured during a request')
+    return """
+        An internal error occured: <pre>{}</pre>
+        See logs for full stacktrace.
+        """.format(e), 500
+
+
